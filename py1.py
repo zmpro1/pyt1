33e06d98300f07771aafb3ab988502734796750d
111 <111@qq.com>
2018-07-05 15:42:19 +0800

    添加了一个忽略文件

---
+++ py1.py
@@ -0,0 +1,22 @@
+a = "123"
+
+print("123")
+
+a = 100
+
+print(a)
+
+name = "Tom"
+age =10
+qq = "123123123123"
+address = "fgdfgfdg"
+
+print("age is %d" %age)
+print("name is %s" %name)
+print("qq is %s" %qq)
+print("company is %s" %address)
+print("age is %d , name is %s" %(age,name))
+
+a,b = 1,2
+a,b = b,a
+print(a)
